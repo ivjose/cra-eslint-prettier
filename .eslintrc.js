ced55ae71950d2d8d063284262a962188a00771f
ivjose <ivjosesantos@gmail.com>
2019-06-16 22:12:22 +0800

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+      'airbnb',
+      'plugin:react/recommended', // uses react-specific linting rules
+      'plugin:prettier/recommended', // enables eslint-plugin-prettier and eslint-config-prettier
+      'prettier/react', // disables react-specific linting rules that conflict with prettier
+
+  ],
+  parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      ecmaFeatures: {
+          jsx: true,
+      },
+      project: './tsconfig.json',
+  },
+  settings: {
+      react: {
+          version: 'detect',
+      },
+  },
+  'env': {
+      'browser': true,
+      'jest': true
+  },
+  rules: {
+    "react/jsx-filename-extension": "off",
+
+    "max-len": ["error", {"code": 100}],
+    "prefer-promise-reject-errors": ["off"],
+    "react/jsx-filename-extension": ["off"],
+    "react/prop-types": ["warn"],
+    "no-return-assign": ["off"]
+  }
+}
